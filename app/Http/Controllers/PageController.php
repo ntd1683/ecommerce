2342741e2a976b9d0c055707db04932bb47d2814
Ntd 1683 <ntdvlog1683@gmail.com>
2023-07-20 18:40:21 +0700

    Refactor

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,27 +9,27 @@ class PageController extends Controller
 {
     public function aboutUs(): View
     {
-        return view('user/page/aboutUs');
+        return view('user.page.aboutUs');
     }
 
     public function contactUs(): View
     {
-        return view('user/page/contactUs');
+        return view('user.page.contactUs');
     }
 
     public function cart(): View
     {
-        return view('user/page/cart');
+        return view('user.page.cart');
     }
 
     public function account(Request $request): View
     {
         $tab = $request->get('tab') ?: 'dashboard';
-        return view('user/page/account', compact('tab'));
+        return view('user.page.account', compact('tab'));
     }
 
     public function shopGridLeftSideBar(): view
     {
-        return view('user/page/shopGridLeftSideBar');
+        return view('user.page.shopGridLeftSideBar');
     }
 }
